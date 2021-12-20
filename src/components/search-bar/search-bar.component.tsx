017716c6571e25d5ed3b13ddc2665091bd309a35
Cristian Tamas <cristiantamas05@gmail.com>
2021-12-20 20:02:45 +0200

    First commit

---
+++ src/components/search-bar/search-bar.component.tsx
@@ -0,0 +1,38 @@
+import { Component, Prop, Event, h, getAssetPath, EventEmitter, State } from '@stencil/core';
+
+@Component({
+  tag: 'search-bar',
+  styleUrl: './search-bar.styles.scss',
+})
+export class SearchBar {
+  @Prop() inputClass: string;
+  @Prop() buttonClass: string;
+
+  @State() searchText: string;
+
+  @Event() emitSearchText: EventEmitter<string>;
+
+  handleChange = event => {
+    this.searchText = event.target.value;
+    console.log(this.searchText);
+  };
+
+  handleEmit = () => {
+    console.log('Current text: ', this.searchText);
+    this.emitSearchText.emit(this.searchText);
+  };
+
+  render() {
+    return (
+      <div class="container">
+        <div class={`input-container ${this.inputClass}`}>
+          <img src={getAssetPath(`../../assets/ibm_carbon_icons/search_glass.svg`)} width="24" height="24" />
+          <input placeholder="Search Products" value={this.searchText} onInput={this.handleChange} />
+        </div>
+        <button class={`${this.buttonClass}`} onClick={this.handleEmit}>
+          Search
+        </button>
+      </div>
+    );
+  }
+}
---
+++ stencil.config.ts
@@ -0,0 +1,36 @@
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+
+
+export const config: Config = {
+  namespace: 'search-configuration-web-components',
+  globalStyle: './src/assets/global.scss',
+  buildEs5: true,
+  minifyJs: false,
+  extras: {
+    cssVarsShim: true,
+    dynamicImportShim: true,
+    shadowDomShim: true,
+    safari10: true,
+    scriptDataOpts: true,
+  },
+  plugins: [
+    sass()
+  ],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
